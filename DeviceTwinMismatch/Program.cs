d356de5d20a916aaa50a2249ceb9a0a5663176a3
Matthijs van der Veer <contact@vanderveer.io>
2019-08-09 08:51:55 +0200

    Initial code commit

---
+++ DeviceTwinMismatch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Newtonsoft.Json;
+
+namespace DeviceTwinMismatch
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Please supply connection string as the only argument.");
+                Environment.Exit(-1);
+            }
+
+            var connectionString = args[0];
+            var registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            var query = registryManager.CreateQuery("SELECT * FROM devices WHERE properties.desired != properties.reported", 100);
+            while (query.HasMoreResults)
+            {
+                var page = await query.GetNextAsTwinAsync();
+                foreach (var twin in page)
+                {
+                     twin.Properties.Desired.ClearMetadata();
+                     twin.Properties.Reported.ClearMetadata();
+                     var reportedAsString = twin.Properties.Reported.ToJson(Formatting.None);
+                     var desiredAsString = twin.Properties.Desired.ToJson(Formatting.None);
+                     if (!string.Equals(reportedAsString, desiredAsString, StringComparison.OrdinalIgnoreCase))
+                     {
+                         Console.WriteLine($"Device {twin.DeviceId} has properties that don't match.");
+                     }
+                }
+            }
+
+            Environment.Exit(0);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # iot-hub-device-twin-match
 Checks if there are differences in device twin reported/desired properties while ignoring metadata.
+
+# Usage
+Build the project and run 
+```
+dotnet .\DeviceTwinMismatch <iot hub connection string>
+```
